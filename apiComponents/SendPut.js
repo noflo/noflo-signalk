67142270ebc49d9c1af11a30da60be129205bb9b
Henri Bergius <henri.bergius@iki.fi>
2022-12-29 20:57:32 +0100

    Component for making PUT requests

---
+++ apiComponents/SendPut.js
@@ -0,0 +1,38 @@
+const noflo = require('noflo');
+
+exports.getComponent = (app) => {
+  return () => {
+    const c = new noflo.Component();
+    c.description = 'Send a Signal K PUT request';
+    c.icon = 'send';
+    c.inPorts.add('path', {
+      datatype: 'string',
+      control: true,
+    });
+    c.inPorts.add('value', {
+      datatype: 'all',
+    });
+    c.outPorts.add('out', {
+      datatype: 'array',
+    });
+    c.forwardBrackets = {
+      value: ['out'],
+    };
+    c.process((input, output) => {
+      if (!input.hasData('path', 'value')) {
+        return;
+      }
+      const [path, value] = input.getData('path', 'value');
+      app.putSelfPath(path, value, (res) => {
+        if (res.state === 'COMPLETED') {
+          output.sendDone({
+            out: res,
+          });
+          return;
+        }
+      });
+    });
+    return c;
+  };
+};
+
