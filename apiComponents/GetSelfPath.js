274acc1c7dc2d2bf3c9c7107f7696296521bb6f5
Henri Bergius <henri.bergius@iki.fi>
2022-12-29 20:28:03 +0100

    More components

--- apiComponents/GetSelfPath.js
+++ apiComponents/GetSelfPath.js
@@ -3,20 +3,22 @@ const noflo = require('noflo');
 exports.getComponent = (app) => {
   return () => {
     const c = new noflo.Component();
-    c.description = 'Get a list of paths currently available in the server';
-    c.icon = 'list';
+    c.description = 'Get a Signal K path for the vessels.self\'s full data model';
+    c.icon = 'indent';
     c.inPorts.add('in', {
-      datatype: 'bang',
+      datatype: 'string',
+      description: 'Path to fetch',
     });
     c.outPorts.add('out', {
-      datatype: 'array',
+      datatype: 'all',
     });
     c.process((input, output) => {
-      input.getData('in');
+      const path = input.getData('in');
       output.sendDone({
-        out: app.streambundle.getAvailablePaths()
+        out: app.getSelfPath(path),
       });
     });
     return c;
   };
 };
+
