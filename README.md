6f934267c1dfe3fa52cfef31ebb810d4accbb70b
Henri Bergius <henri.bergius@iki.fi>
2022-12-29 19:19:07 +0100

    Highly initial

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ NoFlo Signal K plugin
 =====================
 
 This plugin can be used for creating various automations in your [Signal K](http://signalk.org) environment using the [NoFlo](https://noflojs.org) flow-based programming framework.
+
+A similar setup also [exists for Node Red](https://github.com/SignalK/signalk-node-red).
+
+This package supplies a set of NoFlo components for interacting with the [Signal K Server API](https://github.com/SignalK/signalk-server/blob/master/SERVERPLUGINS.md#server-api-for-plugins). For other logic you can either [write your own components](https://noflojs.org/documentation/components/) or install any of the dozens of [NoFlo component libraries](https://www.npmjs.com/search?q=keywords:noflo) available.
---
+++ index.js
@@ -0,0 +1,172 @@
+const server = require('noflo-nodejs');
+const noflo = require('noflo');
+const nofloServer = require('noflo-nodejs/src/server');
+const fbpGraph = require('fbp-graph');
+const { v4: uuidv4 } = require('uuid');
+const { readdir, mkdir } = require('node:fs/promises');
+const path = require('path');
+
+function ensureGraphs(baseDir) {
+  const graphDir = path.resolve(baseDir, './graphs');
+  return readdir(graphDir)
+    .catch((err) => {
+      if (err.code == 'ENOENT') {
+        return mkdir(graphDir)
+          .then(() => {
+            return [];
+          });
+      }
+      throw err;
+    })
+    .then((res) => {
+      if (!res.length) {
+        // No graphs, create a "main"
+        const graphPath = path.resolve(graphDir, 'main.json');
+        const graph = fbpGraph.graph.createGraph('main');
+        graph.setProperties({
+          environment: {
+            type: 'noflo-nodejs',
+          },
+        });
+        return graph.save(graphPath)
+          .then(() => {
+            return [
+              graphPath,
+            ];
+          });
+      }
+      return res.map(r => path.resolve(graphDir, r));
+    });
+}
+
+module.exports = (app) => {
+  let runtime = null;
+  let runtimeConfig = null;
+  let plugin = {};
+
+  plugin.id = 'noflo-signalk';
+  plugin.name = 'NoFlo Signal K';
+  plugin.description = 'Signal K automation with the NoFlo visual programming framework';
+  let skUuid = app.getSelfPath('uuid');
+  if (skUuid) {
+    skUuid = skUuid.split(':').pop();
+  }
+
+  function preStart(rt, config) {
+    runtimeConfig = config;
+    // TODO: Custom component loading with app context here
+    return Promise.resolve();
+  }
+
+  plugin.start = (options, restartPlugin) => {
+    const port = options.port || 3569;
+    // FIXME: Determine whether to use HTTPS or HTTP
+    const ide = options.ide || 'https://app.flowhub.io';
+
+    // We need to use the .signalk directory as baseDir to be able to load components
+    const baseDir = path.resolve(app.getDataDirPath(), '../../');
+
+    // TODO: Load a list of locally stored graphs
+    // TODO: Provide first graph path
+    const config = {
+      id: options.uuid,
+      label: `NoFlo on ${app.getSelfPath('name')}`,
+      secret: options.secret,
+      open: false,
+      autoSave: true,
+      trace: options.trace,
+      protocol: options.protocol || 'websocket',
+      ide,
+      baseDir,
+    };
+    ensureGraphs(baseDir)
+      .then((graphs) => {
+          server(graphs[0], config, preStart)
+        .then((rt) => {
+          runtime = rt;
+          app.setPluginStatus(`NoFlo runtime running in port ${port}`);
+          // TODO: Start all graphs
+        }, (err) => {
+          app.debug(err);
+          console.log(err);
+          app.setPluginError(`Failed to start NoFlo runtime: ${err.message}`);
+        });
+      });
+  };
+
+  plugin.registerWithRouter = (router) => {
+    router.get('/url', (req, res) => {
+      if (!runtime) {
+        res.sendStatus(404);
+        return;
+      }
+      // FIXME: We need options here
+      res.send(nofloServer.liveUrl(runtimeConfig));
+    });
+  };
+
+  plugin.stop = () => {
+    if (!runtime) {
+      return;
+    }
+    app.debug('Stopping NoFlo runtime');
+    // TODO: Stop running NoFlo networks as well
+    nofloServer.stop(runtime)
+      .then(() => {
+        app.debug('NoFlo runtime stopped');
+        runtime = null;
+      }, (err) => {
+        app.debug('Failed to stop the NoFlo runtime');
+        app.debug(err);
+        return;
+      });
+  };
+
+  plugin.schema = {
+    type: 'object',
+    required: [
+      'uuid',
+      'secret',
+    ],
+    properties: {
+      uuid: {
+        title: 'Server instance UUID',
+        type: 'string',
+        format: 'uuid',
+        default: skUuid || uuidv4(),
+      },
+      secret: {
+        title: 'Server instance password',
+        type: 'string',
+        default: uuidv4(),
+      },
+      ide: {
+        title: 'Flowhub / NoFlo UI instance URL',
+        type: 'string',
+        format: 'uri',
+        default: 'https://app.flowhub.io',
+      },
+      protocol: {
+        title: 'FBP protocol transport to use',
+        type: 'string',
+        enum: [
+          'websocket',
+          'webrtc',
+        ],
+        default: 'websocket',
+      },
+      port: {
+        title: 'FBP Protocol port for the IDE to connect to',
+        type: 'number',
+        default: 3569,
+      },
+      trace: {
+        title: 'Whether to capture and store a Flowtrace for each graph',
+        type: 'boolean',
+        default: false,
+      },
+    },
+  };
+
+  return plugin;
+};
