3c046ee7f9de7d6d7d2d975ce68f5bfe7ec451a3
Henri Bergius <henri.bergius@iki.fi>
2024-06-11 16:46:07 +0100

    Use control port for values to allow differing timings

--- components/All.js
+++ components/All.js
@@ -5,18 +5,26 @@ exports.getComponent = () => {
   c.description = 'Checks values of all connected inputs and sends whether they\'re all truthy or falsy';
   c.icon = 'search';
   c.inPorts.add('in', {
+    datatype: 'bang',
+  });
+  c.inPorts.add('values', {
     datatype: 'boolean',
     addressable: true,
+    control: true,
   });
   c.outPorts.add('out', {
     datatype: 'boolean',
   });
   c.process((input, output) => {
-    const indexesWithData = input.attached('in').filter((idx) => input.hasData(['in', idx]));
-    if (indexesWithData.length !== input.attached('in').length) {
+    if (!input.hasData('in')) {
       return;
     }
-    const values = indexesWithData.map((idx) => input.getData(['in', idx]));
+    const indexesWithData = input.attached('values').filter((idx) => input.hasData(['values', idx]));
+    if (indexesWithData.length !== input.attached('values').length) {
+      return;
+    }
+    input.getData('in');
+    const values = indexesWithData.map((idx) => input.getData(['values', idx]));
     output.sendDone({
       // eslint-disable-next-line eqeqeq
       out: values.every((v) => v != false),
